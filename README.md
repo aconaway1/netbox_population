0904e70ecb5078793219042affffc0a60eab26d9
Aaron Conaway <aconaway@aconaway.com>
2024-06-25 14:36:05 -0400

    - Updated `README.md` - Added `racks` and `devices` sections to `sites` section of ``baseline.yml` -- Loads racks and devices that are declared in `sites`

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# Populate Netbox
-Some stuff.
+# Summary
+Some scripts to populate Netbox. This is a separate project from the other 84829294 projects I have that do the same thing.
+
+# Prerequisites
+
+```
+% .venv/bin/pip freeze
+certifi==2024.2.2
+charset-normalizer==3.3.2
+idna==3.7
+packaging==23.2
+pynetbox==7.3.3
+PyYAML==6.0.1
+requests==2.32.3
+urllib3==2.2.1
+```
+
+You need to generate a file called `creds.yml` and put it in the root of the repo. This will contain the Netbox host 
+info and the token used to access it. The contents should look like this.
+
+```
+host: <MY_NB_HOSTNAME>
+token: <MY_API_TOKEN>
+```
+
+You also need to update the `baseline.yml` file with your data. There is some sample date in there to follow. Since one
+piece of information depends on others in Netbox, you'll need to make sure you have the low-level stuff created. 
+```
+manufacturers:
+device_roles:
+device_types:
+sites:
+```
+See the grander Netbox docs to understand how that works.
+
+Under `sites:`, we have `name`, `description`, and `physical_address`. I think you can figure that part out.
+
+Also under `sites:`, we have `racks:` and `devices:`.
+
+```
+...
+    racks:
+      - name: <RACKNAME>
+      - name: <ANOTHER_RACKNAME>
+        u_height: <RU_HEIGHT>  *optional*
+...
+    devices:
+      - name: <DEVICENAME>
+        role: <DEVICE_ROLE> *see roles section*
+        device_type: <DEVICE_TYPE> *see device types section*
+        rack:                      -|
+          name: <RACKNAME>          |- *optional section*
+          position: <RU_POSITION>  -|
+```
+There's some checking going on, so the whole thing won't crash if you mess up the YAML. Don't take that as a challenge,
+please. :)
+
+# Usage
+
+## Loading the baseline
+`% .venv/bin/python populate_baseline.py`
+
+This will load in all baseline info from `baseline.yml` and shove it into Netbox using the creds given.
--- populate_baseline.py
+++ populate_baseline.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     # Load the manufacturers
     for manufacturer in baseline_data['manufacturers']:
         if not manufacturer.get('name'):
-            print(f"This is wrong: {manufacturer}")
+            print(f"This manufacturer is wrong: {manufacturer}")
             continue
 
         man_name = manufacturer['name'].upper()
@@ -39,12 +39,10 @@ if __name__ == "__main__":
         constructed_man = {"name": man_name, "slug": man_slug}
         added_man = nb_conn.dcim.manufacturers.create(constructed_man)
 
-        print(f"{added_man=}")
-
-        # Load the device roles
+    # Load the device roles
     for dev_role in baseline_data['device_roles']:
         if not dev_role.get('name'):
-            print(f"This is wrong: {dev_role}")
+            print(f"This device role is wrong: {dev_role}")
             continue
 
         dev_role_name = dev_role['name'].upper()
@@ -58,19 +56,14 @@ if __name__ == "__main__":
         constructed_dev_role = {"name": dev_role_name, "slug": dev_role_slug}
         added_dev_role = nb_conn.dcim.device_roles.create(constructed_dev_role)
 
-        print(f"{added_dev_role=}")
-
-
     for dev_type in baseline_data['device_types']:
         if not dev_type.get('model'):
-            print(f"This is wrong: {dev_type}")
+            print(f"This device type is missing 'model': {dev_type}")
             continue
 
         if not dev_type.get('manufacturer'):
-            print(f"Missing a manufacturer: {dev_type}")
+            print(f"This device type is missing 'manufacturer': {dev_type}")
             continue
-
-        print(f"Working on {dev_type}")
 
         dev_type_model = dev_type['model'].upper()
         dev_type_slug = dev_type['model'].lower().replace(" ", "")
@@ -82,9 +75,8 @@ if __name__ == "__main__":
             continue
 
         queried_dev_type = nb_conn.dcim.device_types.get(model=dev_type_model)
-        print(f"{queried_dev_type=}")
         if queried_dev_type:
-            print(f"The device type {dev_type} already exists. Skipping.")
+            print(f"The device type {dev_type['model'].upper()} already exists. Skipping.")
             continue
 
         constructed_dev_type = {"model": dev_type_model, "slug": dev_type_slug, "manufacturer": queried_man.id}
@@ -124,3 +116,42 @@ if __name__ == "__main__":
                         constructed_rack['u_height'] = rack['u_height']
                     working_rack = nb_conn.dcim.racks.create(constructed_rack)
                     print(f"Added rack {working_rack}")
+
+        if site.get('devices'):
+            for device in site['devices']:
+                device_name = device['name'].upper()
+                working_device = nb_conn.dcim.devices.get(name=device_name)
+                constructed_device = {"name": device_name, "site": working_site.id, }
+                if not working_device:
+                    # Get the device type ID
+                    queried_dev_type = nb_conn.dcim.device_types.get(slug=device['device_type'].lower())
+                    if not queried_dev_type:
+                        print(f"Device type not found: {device}")
+                        continue
+                    constructed_device['device_type'] = queried_dev_type.id
+                    print(f"{constructed_device=}")
+                    # Get the role ID
+                    queried_dev_role = nb_conn.dcim.device_roles.get(name=device['role'].upper())
+                    if not queried_dev_role:
+                        print(f"Device role not found: {device}")
+                        continue
+                    constructed_device['role'] = queried_dev_role.id
+
+                    if device.get('rack'):
+                        if not device['rack'].get('name'):
+                            print(f"That rack info looks weird: {device}")
+                        queried_rack = nb_conn.dcim.racks.get(name=device['rack']['name'].upper())
+                        if queried_rack:
+                            if device['rack']['position'] > queried_rack.u_height:
+                                print(
+                                    f"The rack position {device['rack']['position']} is not right for {device['rack']}.")
+                            else:
+                                constructed_device['rack'] = queried_rack.id
+                                constructed_device['position'] = device['rack']['position']
+                                constructed_device['face'] = "front"
+                        else:
+                            print(f"Didn't find this rack info for {device_name}: {device['rack']}")
+
+                    # Create the device
+                    added_device = nb_conn.dcim.devices.create(constructed_device)
+                    print(f"Added device: {added_device}")
